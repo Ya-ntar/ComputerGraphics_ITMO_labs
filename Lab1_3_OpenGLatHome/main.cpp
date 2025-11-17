a1be36e349992b0addd6279dbb22e13526166c6f
Ya_ntar <87314636+Ya-ntar@users.noreply.github.com>
2025-11-18 01:42:52 +0300

    feat(main, model, geometry, assets)
    first attempts at geometry (code mainly repeats one in tutorial so far)

--- Lab1_3_OpenGLatHome/main.cpp
+++ Lab1_3_OpenGLatHome/main.cpp
@@ -2,20 +2,28 @@
 #include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <vector>
+
+#include "geometry.h"
+#include "model.h"
 #include "stb_image_write.h"
 
 #include "tgaimage.h"
 
 
 const TGAColor WHITE = TGAColor(255, 255, 255, 255);
-const TGAColor RED   = TGAColor(255, 0,   0,   255);
+const TGAColor RED = TGAColor(255, 0, 0, 255);
 const auto TGA_FILE_NAME = "output.tga";
-const auto OUTPUT_FILE_NAME = "output.png";
+const auto OUTPUT_FILE_NAME = "output2.png";
+constexpr int width = 1000;
+constexpr int height = 1000;
 
-void convertTGAtoPNG(const char* inFile, const char* outFile) {
+void convertTGAtoPNG(const char* inFile, const char* outFile)
+{
     int w, h, ch;
     unsigned char* data = stbi_load(inFile, &w, &h, &ch, 0);
-    if (!data) {
+    if (!data)
+    {
         printf("Failed to load %s\n", inFile);
         return;
     }
@@ -24,9 +32,57 @@ void convertTGAtoPNG(const char* inFile, const char* outFile) {
     stbi_image_free(data);
 }
 
+void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color)
+{
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
 
-int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
+    for (int x = x0; x <= x1; x++)
+    {
+        float t = (x - x0) / (float)(x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
+        if (steep)
+        {
+            image.set(y, x, color);
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+    }
+}
+
+int main(int argc, char** argv)
+
+{
+    TGAImage image(width, height, TGAImage::RGB);
+    auto model = Model("carrot_soup/carrot_soup.obj");
+
+    for (int i = 0; i < model.nfaces(); i++)
+    {
+        std::vector<int> face = model.face(i);
+        for (int j = 0; j < 3; j++)
+        {
+            Vec3f v0 = model.vert(face[j]);
+            Vec3f v1 = model.vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, WHITE);
+        }
+    }
+
     image.set(52, 41, RED);
     image.flip_vertically();
 
@@ -35,4 +91,3 @@ int main(int argc, char** argv) {
     convertTGAtoPNG(TGA_FILE_NAME, OUTPUT_FILE_NAME);
     return 0;
 }
-
